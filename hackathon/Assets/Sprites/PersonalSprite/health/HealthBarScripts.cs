49a0f1d981a67e45f19807aaf0e678704d782e47
stolenlegos <stolenlegos@gmail.com>
2021-10-06 17:39:23 -0400

    crap

--- hackathon/Assets/Sprites/PersonalSprite/health/HealthBarScripts.cs
+++ hackathon/Assets/Sprites/PersonalSprite/health/HealthBarScripts.cs
@@ -1,39 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class HealthBarScripts : MonoBehaviour
 {
     public Slider slider;
+    public PlayerEffects player;
     private int _maxHealth;
-    private int _currentHealth; 
-    private bool _loseStateReached = false; 
+    private int _currentHealth;
+    private bool _loseStateReached = false;
     public void SetMaxHealth(int health) {
-        slider.maxValue = health; 
+        slider.maxValue = health;
         slider.value = health;
         _maxHealth = health;
-        _currentHealth = health;  
-    } 
+        _currentHealth = health;
+    }
     public void SetHealth(int health) {
         slider.value = health;
-        _currentHealth = health; 
+        _currentHealth = health;
     }
 
     public void AddHealth(int health) {
-        slider.value += health; 
+        slider.value += health;
         _currentHealth += health;
     }
 
     public void SubHealth(int health) {
-        slider.value -= health; 
+        slider.value -= health;
         _currentHealth -= health;
     }
 
-    private void Update() { 
-        if (_currentHealth <= 0 && !_loseStateReached) {
-            Time.timeScale = 0; 
-            _loseStateReached = true; 
-        }
+    private void Update() {
+        slider.value = player.health;
     }
 }
